8bdc3c9bb4e116e56e57fb1d78a32fa5f945a904
Fernando Mendoza <55853779+whiteknight-dev@users.noreply.github.com>
2025-03-14 20:49:41 -0600

    Done Responsive Layout and Active States

--- main.js
+++ main.js
@@ -1,14 +1,45 @@
+let width = window.innerWidth;
 const cardFooter = document.querySelector(".card__footer");
 const cardShare = document.querySelector(".card__share");
-const primaryButton = document.querySelector(".primary");
-const secondaryButton = document.querySelector(".secondary");
+let primaryButton = null;
+let secondaryButton = null;
+let svg = null;
 
-primaryButton.addEventListener("click", function () {
-  cardFooter.classList.add("hidden");
-  cardShare.classList.remove("hidden");
-});
+function bindEvents() {
+  if (primaryButton !== null && secondaryButton !== null) {
+    primaryButton.replaceWith(primaryButton.cloneNode(true));
+    secondaryButton.replaceWith(secondaryButton.cloneNode(true));
+  }
 
-secondaryButton.addEventListener("click", function () {
-  cardShare.classList.add("hidden");
-  cardFooter.classList.remove("hidden");
-});
+  svg = document.getElementById("svg");
+  primaryButton = document.querySelector(".primary");
+  secondaryButton = document.querySelector(".secondary");
+
+  let width = window.innerWidth;
+  if (width < 768) {
+    primaryButton.addEventListener("click", function () {
+      cardFooter.classList.add("hidden");
+      cardShare.classList.remove("hidden");
+    });
+
+    secondaryButton.addEventListener("click", function () {
+      cardShare.classList.add("hidden");
+      cardFooter.classList.remove("hidden");
+    });
+  } else {
+    primaryButton.addEventListener("click", function () {
+      cardShare.classList.toggle("hidden");
+      if (this.classList.contains("active")) {
+        this.classList.remove("active");
+        svg.style.fill = "#6E8098";
+      } else {
+        this.classList.add("active");
+        svg.style.fill = "#fff";
+      }
+    });
+  }
+}
+
+bindEvents();
+
+addEventListener("resize", bindEvents);
