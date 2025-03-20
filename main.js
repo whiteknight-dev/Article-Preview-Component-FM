8ca37d1792d172fe6bc45527985ba61ef60b4c2c
Fernando Mendoza <55853779+whiteknight-dev@users.noreply.github.com>
2025-03-20 13:45:40 -0600

    Remove Unnecessary JS and Implement a CSS Alternative

--- main.js
+++ main.js
@@ -1,45 +1,11 @@
-let width = window.innerWidth;
+const cardFooterSection = document.querySelector(".card__footer-section");
 const cardFooter = document.querySelector(".card__footer");
 const cardShare = document.querySelector(".card__share");
-let primaryButton = null;
-let secondaryButton = null;
-let svg = null;
+let primaryButton = document.querySelector(".share-button");
 
-function bindEvents() {
-  if (primaryButton !== null && secondaryButton !== null) {
-    primaryButton.replaceWith(primaryButton.cloneNode(true));
-    secondaryButton.replaceWith(secondaryButton.cloneNode(true));
-  }
-
-  svg = document.getElementById("svg");
-  primaryButton = document.querySelector(".primary");
-  secondaryButton = document.querySelector(".secondary");
-
-  let width = window.innerWidth;
-  if (width < 768) {
-    primaryButton.addEventListener("click", function () {
-      cardFooter.classList.add("hidden");
-      cardShare.classList.remove("hidden");
-    });
-
-    secondaryButton.addEventListener("click", function () {
-      cardShare.classList.add("hidden");
-      cardFooter.classList.remove("hidden");
-    });
-  } else {
-    primaryButton.addEventListener("click", function () {
-      cardShare.classList.toggle("hidden");
-      if (this.classList.contains("active")) {
-        this.classList.remove("active");
-        svg.style.fill = "#6E8098";
-      } else {
-        this.classList.add("active");
-        svg.style.fill = "#fff";
-      }
-    });
-  }
-}
-
-bindEvents();
-
-addEventListener("resize", bindEvents);
+primaryButton.addEventListener("click", () => {
+  primaryButton.classList.toggle("active");
+  cardFooterSection.classList.toggle("active");
+  cardFooter.classList.toggle("hidden");
+  cardShare.classList.toggle("visible");
+});
